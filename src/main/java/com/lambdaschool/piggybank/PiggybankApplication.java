360bcc2b623e740d12f927f5de31a4ed8b05bb77
pak11273 <pak11273@gmail.com>
2021-03-12 05:17:26 -0600

    added comment

--- src/main/java/com/lambdaschool/piggybank/PiggybankApplication.java
+++ src/main/java/com/lambdaschool/piggybank/PiggybankApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PiggybankApplication
 {
-
+    // public static void entry point
     public static void main(String[] args)
     {
         SpringApplication.run(PiggybankApplication.class, args);
